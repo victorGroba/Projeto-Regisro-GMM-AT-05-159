0102291313d83496b6bf1920433bea6b09f64274
victorGroba <victorgroba2@gmail.com>
2026-01-12 12:47:58 -0300

    atualização do init

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,25 @@
 from flask import Flask
 from flask_login import LoginManager
 from .models import db, User
+import os # <--- Não esqueça de importar o OS
 
 def create_app():
     app = Flask(__name__)
-    # Altere esta chave para algo seguro em produção
-    app.config['SECRET_KEY'] = 'chave_secreta_projeto_balanca' 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///balancas.db'
+    app.config['SECRET_KEY'] = 'sua_chave_secreta_aqui'
+    
+    # --- CORREÇÃO AQUI ---
+    # Usamos 4 barras (////) para indicar um caminho absoluto no Linux/Docker
+    # Isso garante que ele pegue o arquivo dentro da pasta instance onde seus dados estão salvos
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////app/instance/balancas.db' 
+    # ---------------------
+
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
 
-    # Configuração do Login
     login_manager = LoginManager()
     login_manager.login_view = 'routes.login'
-    login_manager.login_message = "Por favor, faça login para acessar esta página."
-    login_manager.login_message_category = "warning"
+    login_manager.login_message = "Faça login para acessar."
     login_manager.init_app(app)
 
     @login_manager.user_loader
@@ -24,5 +28,9 @@ def create_app():
 
     from .routes import routes_bp
     app.register_blueprint(routes_bp)
+    
+    # Garante que as tabelas existam (cria se não houver)
+    with app.app_context():
+        db.create_all()
 
     return app
